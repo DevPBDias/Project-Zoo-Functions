038378d5379861d49583231a2d743ec49c827fea
devpbdias <devpbdias@gmail.com>
2022-03-17 11:38:05 -0300

    add 2nd req e refatoramento 1st req

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,10 +1,15 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 // Esta função, a partir do nome de uma espécie e uma idade mínima, verifica se todos os animais daquela espécie possuem a idade mínima especificada.
 // Deve retornar um valor booleano.
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const findingAnimal = species.find((elementoF) => elementoF.name === animal);
+  const result = findingAnimal.residents.every((elemento) => elemento.age >= age);
+  return result;
+  // tenho que chegar no residents e dps no age dentro dele
+  // verificar a age com a idade minima (>=)
 }
 
+getAnimalsOlderThan('otters', 7);
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,9 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 // Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
 
 function getSpeciesByIds(...array) {
-  return data.species.filter((elemento) => {
-    return array.find((elementFind) => elementFind === elemento.id);
-  });
+  return species.filter((elemento) => array.find((elementFind) => elementFind === elemento.id));
 }
 
 getSpeciesByIds();
